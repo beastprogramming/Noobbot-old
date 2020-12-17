caea173ff1c725b5e3f2fc8df55898ca5a7b58ef
Himanshu Jain <himanshu@noobbot.com>
2020-12-17 22:26:54 +0530

    Add dropdown fuctionality to main menu

--- src/components/menus/MainMenu.js
+++ src/components/menus/MainMenu.js
@@ -1,17 +1,22 @@
+import React, { useState } from "react";
 import {BrowserRouter as Router,Link} from "react-router-dom";
+
 function MainMenu() {
+const [mainMenu, setMainMenu] = useState(false)
+const handleMainMenu = () => {setMainMenu(!mainMenu)}    
+
     return(
         <>
         <div className="nb-main-menu nb-pl-2 xl:nb-pl-8 nb-pr-2 xl:nb-pr-0 xl:nb-block nb-hidden nb-shadow">
             <ul className="nb-flex nb-flex-col lg:nb-flex-row lg:nb-items-center lg:nb-justify-between nb-text-gray-800">
                 <Router>
-                    <li className="nb-border-b lg:nb-border-b-0 nb-border-gray-300">
+                    <li className="nb-border-b lg:nb-border-b-0 nb-border-gray-300" onClick={handleMainMenu}>
 
                         <div className="nb-is-mega-menu nb-pr-2 nb-py-2 nb-text-md nb-capitalize nb-font-bold nb-cursor-pointer">
                         Domains
-                        <i className="las la-angle-down"></i>
+                        <i className={mainMenu ?  "las la-angle-up nb-ml-1" : "las la-angle-down nb-ml-1"}></i>
 
-                        <div className="nb-mega-menu nb-bg-white nb-absolute nb-left-0 nb-right-0 nb-p-4 nb-pl-2 xl:nb-pl-8">
+                        <div className={`nb-mega-menu nb-bg-white nb-absolute nb-left-0 nb-right-0 nb-p-4 nb-pl-2 xl:nb-pl-8 ${mainMenu ?  "nb-block" : "nb-hidden"}`}>
                             <div className="nb-w-full nb-grid nb-grid-col-1 lg:nb-grid-cols-4 nb-h-full nb-normal-case">
 
                                 <div className="lg:nb-col-span-1 nb-bg-gray-200 nb-px-4 nb-py-8">
