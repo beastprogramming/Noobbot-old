321dfdb229c55c3b23ae3d296ec44d399e5d5bb6
Himanshu Jain <himanshu@noobbot.com>
2020-12-19 18:41:58 +0530

    MainMenu Updated

--- src/components/menus/MainMenu.js
+++ src/components/menus/MainMenu.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Link } from "react-router-dom";
 
 function MainMenu() {
   // Menu DropDown
-  const [mainMenu, setMainMenu] = useState(false);
-  const handleMainMenu = () => {
-    setMainMenu(!mainMenu);
+  // const [mainMenu, setMainMenu] = useState(false);
+  const handleMainMenu = (e) => {
+    let clickedItem = e.currentTarget;
+    clickedItem.querySelector(".nb-mega-menu").classList.toggle("nb-block");
+    clickedItem.querySelector(".nb-mega-menu").classList.toggle("nb-hidden");
   };
 
   // Fetching menuitems data
@@ -34,24 +36,12 @@ function MainMenu() {
                 >
                   {menuItem.text}
                   {menuItem.children ? (
-                    <i
-                      className={
-                        menuItem.children && mainMenu
-                          ? "las la-angle-up nb-ml-1"
-                          : menuItem.children ?
-                          "las la-angle-down nb-ml-1"
-                          : null
-                      }
-                    ></i>
+                    <i className="las la-angle-down nb-ml-1"></i>
                   ) : (
                     ""
                   )}
                   {menuItem.children ? (
-                    <div
-                      className={`nb-mega-menu nb-bg-white nb-absolute nb-left-0 nb-right-0 nb-p-4 nb-pl-2 xl:nb-pl-8 nb-mt-2 ${
-                        mainMenu ? "nb-block" : "nb-hidden"
-                      }`}
-                    >
+                    <div className="nb-mega-menu nb-bg-white nb-absolute nb-left-0 nb-right-0 nb-p-4 nb-pl-2 xl:nb-pl-8 nb-mt-2 nb-hidden">
                       <div className="nb-w-full nb-grid nb-grid-col-1 lg:nb-grid-cols-4 nb-h-full nb-normal-case">
                         <div className="lg:nb-col-span-1 nb-bg-gray-200 nb-px-4 nb-py-8">
                           <h2 className="nb-text-2xl nb-font-bold nb-mb-2">
@@ -72,33 +62,31 @@ function MainMenu() {
 
                         <div className="lg:nb-col-span-3 nb-p-4">
                           <div className="nb-grid nb-grid-cols-3 nb-gap-4">
-                          {menuItem.children.map((megaMenu) => (
-                            <Link
-                              key={megaMenu.name}
-                              to="#"
-                              title={megaMenu.title}
-                              className="nb-flex nb-justify-center nb-text-left nb-items-center nb-text-gray-800 hover:nb-text-gray-900"
-                            >
-                              <div className="nb-w-1-4">
-                                <i className={`${megaMenu.icon} la-3x`}></i>
-                              </div>
-                              <div className="nb-w-3/4 nb-pl-2">
-                                <h2 className="nb-text-md nb-font-bold">
-                                  {megaMenu.name}
-                                </h2>
-                                <p className="nb-text-sm nb-font-medium nb-opacity-75">
-                                  {megaMenu.description}
-                                </p>
-                              </div>
-                            </Link>
+                            {menuItem.children.map((megaMenu) => (
+                              <Link
+                                key={megaMenu.name}
+                                to="#"
+                                title={megaMenu.title}
+                                className="nb-flex nb-justify-center nb-text-left nb-items-center nb-text-gray-800 hover:nb-text-gray-900"
+                              >
+                                <div className="nb-w-1-4">
+                                  <i className={`${megaMenu.icon} la-3x`}></i>
+                                </div>
+                                <div className="nb-w-3/4 nb-pl-2">
+                                  <h2 className="nb-text-md nb-font-bold">
+                                    {megaMenu.name}
+                                  </h2>
+                                  <p className="nb-text-sm nb-font-medium nb-opacity-75">
+                                    {megaMenu.description}
+                                  </p>
+                                </div>
+                              </Link>
                             ))}
                           </div>
                         </div>
                       </div>
                     </div>
-                  ) : (
-                    null
-                  )}
+                  ) : null}
                 </div>
               </li>
             ))}
