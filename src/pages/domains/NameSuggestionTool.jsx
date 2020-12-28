a9860e05a6ce783739b72487f8b08880309911d5
Himanshu Jain <himanshu@noobbot.com>
2020-12-28 19:07:26 +0530

    Setup Proxy

--- src/pages/domains/NameSuggestionTool.jsx
+++ src/pages/domains/NameSuggestionTool.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 
 function NameSuggestionTool() {
+
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        fetch(`/api/domains/v5/suggest-names.json?auth-userid=795300&api-key=sOZ7I52D5Jx8Vtbkxfnn1PYIVoqL0D5O&keyword=rohit`)
+        .then((response) => response.json())
+	    .then(setSuggestions);
+    }, []);
+
     return(
         <>
-            <h1>Name Suggestion Tool</h1>
+            
         </>
     )
 }
