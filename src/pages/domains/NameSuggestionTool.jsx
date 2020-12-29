767fcd0af61a371a1bff68a6a56e5ccebb3a3d5e
Himanshu Jain <himanshu@noobbot.com>
2020-12-29 19:13:01 +0530

    API Data fetched

--- src/pages/domains/NameSuggestionTool.jsx
+++ src/pages/domains/NameSuggestionTool.jsx
@@ -2,18 +2,35 @@ import React, { useState, useEffect} from "react";
 
 function NameSuggestionTool() {
 
-    const [suggestions, setSuggestions] = useState([]);
+    const [suggestions, setSuggestions] = useState({});
 
     useEffect(() => {
-        fetch(`/api/domains/v5/suggest-names.json?auth-userid=795300&api-key=sOZ7I52D5Jx8Vtbkxfnn1PYIVoqL0D5O&keyword=rohit`)
+        fetch(`/api/domains/v5/suggest-names.json?auth-userid=795300&api-key=sOZ7I52D5Jx8Vtbkxfnn1PYIVoqL0D5O&keyword=himanshu`)
         .then((response) => response.json())
         .then(setSuggestions);
   }, []);
 
-  var as = JSON.parse(suggestions);
+  const suggestionArray = [];
+  for (let suggestion in suggestions) {
+    let status = suggestions[suggestion];
+    suggestionArray.push({
+        name: suggestion,
+        children: status
+    });
+  }
+
+//   console.log(suggestionArray);
+  
     return(
         <div>
-            {as}
+            {suggestionArray.map((suggest) => {
+                return(
+                    <div  key={suggest.name} >
+                    <p>{suggest.name}</p>
+                    <p className="nb-text-primary-green">{suggest.children.status}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
