2f3682ec308824f50598ed7b6e35b2f801fc1a8f
Himanshu Jain <himanshu@noobbot.com>
2021-01-07 18:07:05 +0530

    Offer update

--- src/pages/misc/offers.jsx
+++ src/pages/misc/offers.jsx
@@ -19,11 +19,21 @@ function Offers() {
     }, [])
     return (
         <>
-        {Object.keys(promos).map((offer, i) => {
+        {/* {Object.keys(promos).map((offer, i) => {
             return(
             <p key={i}>{promos[offer].productkey}</p>
             )
-        })}
+        })} */}
+
+        <section className="nb-py-8 nb-bg-gray-100">
+            <div className="nb-container nb-p-4">
+                <div className="nb-grid nb-grid-cols-1 md:nb-grid-cols-2 lg:nb-grid-cols-4 xl:nb-grid-cols-5 nb-my-8">
+                    <div className="nb-p-4 nb-shadow-lg nb-rounded-md nb-bg-white">
+                        <p className="nb-text-lg nb-font-bold nb-text-primary-bunty nb-pb-2 nb-border-b">.com</p>
+                    </div>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
