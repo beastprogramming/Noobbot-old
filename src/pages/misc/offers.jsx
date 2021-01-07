8d7243da486357f6f11e0162cfec272162e8fffa
Himanshu Jain <himanshu@noobbot.com>
2021-01-07 22:15:53 +0530

    update promo page

--- src/pages/misc/offers.jsx
+++ src/pages/misc/offers.jsx
@@ -19,27 +19,33 @@ function Offers() {
     }, [])
     return (
         <>
-        {/* {Object.keys(promos).map((offer, i) => {
-            return(
-            <p key={i}>{promos[offer].productkey}</p>
-            )
-        })} */}
-
+        
         <section className="nb-py-8 nb-bg-gray-100">
             <div className="nb-container nb-p-4">
-                <div className="nb-grid nb-grid-cols-1 md:nb-grid-cols-2 lg:nb-grid-cols-4 xl:nb-grid-cols-5 nb-my-8">
-                    <div className="nb-shadow-sm hover:nb-shadow-xl nb-rounded-t-sm nb-bg-white">
+                <div className="nb-grid nb-grid-cols-1 md:nb-grid-cols-2 lg:nb-grid-cols-4  nb-gap-4 nb-my-8">
+                {Object.keys(promos).map((offer, i) => {
+                    return(
+                    <div className="nb-shadow-sm hover:nb-shadow-lg nb-rounded-t-sm nb-bg-white" key={i}>
                         <div className="nb-p-4">
-                            <p className="nb-text-lg nb-font-bold nb-text-primary-bunty nb-pb-2 nb-border-b">.com</p>
-                                <div className="nb-pt-2 nb-flex nb-justify-between">
-                                    <p className="nb-text-lg nb-font-bold nb-text-primary-blue">₹390.00 </p>
-                                    <p className="nb-text-md nb-font-bold nb-line-through nb-text-primary-bunty nb-opacity-50">₹1390.00</p>
-                                </div>
+
+                            {Object.keys(domainInfo).map((di, index) => {
+                                return (
+                                    <p className="nb-text-lg nb-font-bold nb-text-primary-bunty">
+                                        { promos[offer].productkey === di ?(domainInfo[di].text): null }
+                                    </p>
+                                )
+                            })}
+                            <div className="nb-pt-2 nb-flex nb-justify-between nb-border-t">
+                                <p className="nb-text-lg nb-font-bold nb-text-primary-blue">₹{promos[offer].customerprice} </p>
+                                <p className="nb-text-md nb-font-bold nb-line-through nb-text-primary-bunty nb-opacity-50">₹{promos[offer].barrierprice}</p>
+                            </div>
                         </div>
                         <Link to="#" title="Apply Offer" target="_self" className="">
                             <div className="nb-p-2 nb-bg-primary-blue  nb-font-bold nb-rounded-b-sm nb-text-white nb-w-auto nb-text-center">Apply Offer</div>
                         </Link>
                     </div>
+                    )
+                })} 
                 </div>
             </div>
         </section>
