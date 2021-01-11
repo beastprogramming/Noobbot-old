8d934b170aeb3bd9a260fe2ffdf5ee488dd0d76b
Himanshu Jain <himanshuj@xcellhost.cloud>
2021-01-11 12:08:42 +0530

    Linux Shared Hosting Page

--- src/components/pricing/PricingStandard.jsx
+++ src/components/pricing/PricingStandard.jsx
@@ -1,123 +1,152 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function PricingStandard() {
-    return (
-        <>
-        <section className="nb-py-4">
-            <div className="nb-container nb-my-4 md:nb-my-8 lg:nb-my-16">
-                <div className="nb-text-center nb-my-4 md:nb-my-8 lg:nb-my-12">
-                    <h3 className="nb-text-3xl lg:nb-text-5xl nb-font-bold nb-text-primary-bunty nb-tracking-wide">Get your business online today
-                    </h3>
-                    <h4 className="nb-text-xl lg:nb-text-3xl nb-font-semibold nb-text-primary-blue nb-tracking-wide">99% uptime for rock-solid
-                        performance
-                    </h4>
-
+  return (
+    <>
+      <section className="nb-py-4">
+        <div className="nb-container nb-my-4 md:nb-my-8 lg:nb-my-16">
+          <div className="nb-text-center nb-my-4 md:nb-my-8 lg:nb-my-12">
+            <h3 className="nb-text-3xl lg:nb-text-5xl nb-font-bold nb-text-primary-bunty nb-tracking-wide">
+              Get your business online today
+            </h3>
+            <h4 className="nb-text-xl lg:nb-text-3xl nb-font-semibold nb-text-primary-blue nb-tracking-wide">
+              99% uptime for rock-solid performance
+            </h4>
+          </div>
+          <div className="nb-grid nb-grid-cols-1 md:nb-grid-cols-2 lg:nb-grid-cols-4 nb-gap-4">
+            <div className="nb-border-2 nb-border-primary-blue nb-p-4 nb-rounded">
+              <h1 className="nb-text-2xl nb-font-bold nb-text-primary-bunty nb-text-center">
+                Shared Hosting
+              </h1>
+              <h2 className="nb-text-sm nb-font-semibold nb-opacity-75 nb-text-primary-blue nb-text-center">
+                Free WHMCS Included
+              </h2>
+              <ul className="nb-mt-8 nb-mb-4">
+                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                  <p className="nb-text-sm">99.9% Uptime Guarantee</p>
+                </li>
+                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                  <p className="nb-text-sm">WHM for Management</p>
+                </li>
+                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                  <p className="nb-text-sm">Softaculous Included</p>
+                </li>
+              </ul>
+              <h2 className="nb-text-sm nb-font-semibold nb-text-primary-blue nb-text-center nb-opacity-75">
+                Starting From
+              </h2>
+              <h1 className="nb-mb-4 nb-text-3xl nb-font-semibold nb-text-primary-blue nb-text-center">
+                <span className="nb-text-xl">₹</span>
+                1,080.00
+                <span className="nb-text-xl">per mo</span>
+              </h1>
+              <Link to="" className="">
+                <div className="nb-h-12 nb-rounded nb-border-primary-red nb-border-2 nb-p-2 nb-text-xl nb-font-bold nb-bg-primary-red nb-text-white nb-cursor-pointer nb-text-center">
+                  View Plans
                 </div>
-                <div className="nb-grid nb-grid-cols-1 md:nb-grid-cols-2 lg:nb-grid-cols-4 nb-gap-4">
-                    
-                <div className="nb-border-2 nb-border-primary-blue nb-p-4 nb-rounded">
-                        <h1 className="nb-text-2xl nb-font-bold nb-text-primary-bunty nb-text-center">Shared Hosting</h1>
-                        <h2 className="nb-text-sm nb-font-semibold nb-opacity-75 nb-text-primary-blue nb-text-center">Free WHMCS Included</h2>
-                        <ul className="nb-mt-8 nb-mb-4">
-                            <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
-                                <p className="nb-text-sm">99.9% Uptime Guarantee</p>
-                            </li>
-                            <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
-                                <p className="nb-text-sm">WHM for Management</p>
-                            </li>
-                            <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
-                                <p className="nb-text-sm">Softaculous Included</p>
-                            </li>
-                        </ul>
-                        <h2 className="nb-text-sm nb-font-semibold nb-text-primary-blue nb-text-center nb-opacity-75">Starting From</h2>
-                        <h1 className="nb-mb-4 nb-text-3xl nb-font-semibold nb-text-primary-blue nb-text-center"><span className="nb-text-xl">₹</span>
-                            1,080.00
-                            <span className="nb-text-xl">per mo</span></h1>
-                        <Link to="" className="">
-                            <div
-                                className="nb-h-12 nb-rounded nb-border-primary-red nb-border-2 nb-p-2 nb-text-xl nb-font-bold nb-bg-primary-red nb-text-white nb-cursor-pointer nb-text-center">
-                                View Plans</div>
-                        </Link>
-                    </div>
-                    <div className="nb-border-2 nb-border-primary-blue nb-p-4 nb-rounded">
-                        <h1 className="nb-text-2xl nb-font-bold nb-text-primary-bunty nb-text-center">Reseller Hosting</h1>
-                        <h2 className="nb-text-sm nb-font-semibold nb-opacity-75 nb-text-primary-blue nb-text-center">Free WHMCS Included</h2>
-                        <ul className="nb-mt-8 nb-mb-4">
-                            <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
-                                <p className="nb-text-sm">99.9% Uptime Guarantee</p>
-                            </li>
-                            <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
-                                <p className="nb-text-sm">WHM for Management</p>
-                            </li>
-                            <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
-                                <p className="nb-text-sm">Softaculous Included</p>
-                            </li>
-                        </ul>
-                        <h2 className="nb-text-sm nb-font-semibold nb-text-primary-blue nb-text-center nb-opacity-75">Starting From</h2>
-                        <h1 className="nb-mb-4 nb-text-3xl nb-font-semibold nb-text-primary-blue nb-text-center"><span className="nb-text-xl">₹</span>
-                            1,080.00
-                            <span className="nb-text-xl">per mo</span></h1>
-                        <Link to="" className="">
-                            <div
-                                className="nb-h-12 nb-rounded nb-border-primary-red nb-border-2 nb-p-2 nb-text-xl nb-font-bold nb-bg-primary-red nb-text-white nb-cursor-pointer nb-text-center">
-                                View Plans</div>
-                        </Link>
-                    </div>
-                    <div className="nb-border-2 nb-border-primary-blue nb-p-4 nb-rounded">
-                        <h1 className="nb-text-2xl nb-font-bold nb-text-primary-bunty nb-text-center">SSL Certificate</h1>
-                        <h2 className="nb-text-sm nb-font-semibold nb-opacity-75 nb-text-primary-blue nb-text-center">Secure your Website</h2>
-                        <ul className="nb-mt-8 nb-mb-4">
-                            <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
-                                <p className="nb-text-sm">Upto 256-bit Encryption</p>
-                            </li>
-                            <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
-                                <p className="nb-text-sm">Free Reissues Included</p>
-                            </li>
-                            <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
-                                <p className="nb-text-sm">Browser Compatibility</p>
-                            </li>
-                        </ul>
-                        <h2 className="nb-text-sm nb-font-semibold nb-text-primary-blue nb-text-center nb-opacity-75">Starting From</h2>
-                        <h1 className="nb-mb-4 nb-text-3xl nb-font-semibold nb-text-primary-blue nb-text-center"><span className="nb-text-xl">₹</span>
-                            689.00
-                            <span className="nb-text-xl">per yr</span></h1>
-                        <Link to="" className="">
-                            <div
-                                className="nb-h-12 nb-rounded  nb-border-primary-red nb-border-2 nb-p-2 nb-text-xl nb-font-bold nb-bg-primary-red nb-text-white nb-cursor-pointer nb-text-center">
-                                View Plans</div>
-                        </Link>
-                    </div>
-                    <div className="nb-border-2 nb-border-primary-blue nb-p-4 nb-rounded">
-                        <h1 className="nb-text-2xl nb-font-bold nb-text-primary-bunty nb-text-center">Build your website</h1>
-                        <h2 className="nb-text-sm nb-font-semibold nb-opacity-75 nb-text-primary-blue nb-text-center">No technical skills required
-                        </h2>
-                        <ul className="nb-mt-8 nb-mb-4">
-                            <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
-                                <p className="nb-text-sm">Quick & Easy to Use</p>
-                            </li>
-                            <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
-                                <p className="nb-text-sm">Choose from 100's of designs</p>
-                            </li>
-                            <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
-                                <p className="nb-text-sm">Build Professional Websites</p>
-                            </li>
-                        </ul>
-                        <h2 className="nb-text-sm nb-font-semibold nb-text-primary-blue nb-text-center nb-opacity-75">Starting From</h2>
-                        <h1 className="nb-mb-4 nb-text-3xl nb-font-semibold nb-text-primary-blue nb-text-center"><span className="nb-text-xl">₹</span>
-                            198.00
-                            <span className="nb-text-xl">per mo</span></h1>
-                        <Link to="" className="">
-                            <div
-                                className="nb-h-12 nb-rounded  nb-border-primary-red nb-border-2 nb-p-2 nb-text-xl nb-font-bold nb-bg-primary-red nb-text-white nb-cursor-pointer nb-text-center">
-                                View Plans</div>
-                        </Link>
-                    </div>
-                
-                </div>
+              </Link>
             </div>
-        </section>
-        </>
-    )
+            <div className="nb-border-2 nb-border-primary-blue nb-p-4 nb-rounded">
+              <h1 className="nb-text-2xl nb-font-bold nb-text-primary-bunty nb-text-center">
+                Reseller Hosting
+              </h1>
+              <h2 className="nb-text-sm nb-font-semibold nb-opacity-75 nb-text-primary-blue nb-text-center">
+                Free WHMCS Included
+              </h2>
+              <ul className="nb-mt-8 nb-mb-4">
+                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                  <p className="nb-text-sm">99.9% Uptime Guarantee</p>
+                </li>
+                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                  <p className="nb-text-sm">WHM for Management</p>
+                </li>
+                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                  <p className="nb-text-sm">Softaculous Included</p>
+                </li>
+              </ul>
+              <h2 className="nb-text-sm nb-font-semibold nb-text-primary-blue nb-text-center nb-opacity-75">
+                Starting From
+              </h2>
+              <h1 className="nb-mb-4 nb-text-3xl nb-font-semibold nb-text-primary-blue nb-text-center">
+                <span className="nb-text-xl">₹</span>
+                1,080.00
+                <span className="nb-text-xl">per mo</span>
+              </h1>
+              <Link to="" className="">
+                <div className="nb-h-12 nb-rounded nb-border-primary-red nb-border-2 nb-p-2 nb-text-xl nb-font-bold nb-bg-primary-red nb-text-white nb-cursor-pointer nb-text-center">
+                  View Plans
+                </div>
+              </Link>
+            </div>
+            <div className="nb-border-2 nb-border-primary-blue nb-p-4 nb-rounded">
+              <h1 className="nb-text-2xl nb-font-bold nb-text-primary-bunty nb-text-center">
+                SSL Certificate
+              </h1>
+              <h2 className="nb-text-sm nb-font-semibold nb-opacity-75 nb-text-primary-blue nb-text-center">
+                Secure your Website
+              </h2>
+              <ul className="nb-mt-8 nb-mb-4">
+                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                  <p className="nb-text-sm">Upto 256-bit Encryption</p>
+                </li>
+                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                  <p className="nb-text-sm">Free Reissues Included</p>
+                </li>
+                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                  <p className="nb-text-sm">Browser Compatibility</p>
+                </li>
+              </ul>
+              <h2 className="nb-text-sm nb-font-semibold nb-text-primary-blue nb-text-center nb-opacity-75">
+                Starting From
+              </h2>
+              <h1 className="nb-mb-4 nb-text-3xl nb-font-semibold nb-text-primary-blue nb-text-center">
+                <span className="nb-text-xl">₹</span>
+                689.00
+                <span className="nb-text-xl">per yr</span>
+              </h1>
+              <Link to="" className="">
+                <div className="nb-h-12 nb-rounded  nb-border-primary-red nb-border-2 nb-p-2 nb-text-xl nb-font-bold nb-bg-primary-red nb-text-white nb-cursor-pointer nb-text-center">
+                  View Plans
+                </div>
+              </Link>
+            </div>
+            <div className="nb-border-2 nb-border-primary-blue nb-p-4 nb-rounded">
+              <h1 className="nb-text-2xl nb-font-bold nb-text-primary-bunty nb-text-center">
+                Build your website
+              </h1>
+              <h2 className="nb-text-sm nb-font-semibold nb-opacity-75 nb-text-primary-blue nb-text-center">
+                No technical skills required
+              </h2>
+              <ul className="nb-mt-8 nb-mb-4">
+                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                  <p className="nb-text-sm">Quick & Easy to Use</p>
+                </li>
+                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                  <p className="nb-text-sm">Choose from 100's of designs</p>
+                </li>
+                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                  <p className="nb-text-sm">Build Professional Websites</p>
+                </li>
+              </ul>
+              <h2 className="nb-text-sm nb-font-semibold nb-text-primary-blue nb-text-center nb-opacity-75">
+                Starting From
+              </h2>
+              <h1 className="nb-mb-4 nb-text-3xl nb-font-semibold nb-text-primary-blue nb-text-center">
+                <span className="nb-text-xl">₹</span>
+                198.00
+                <span className="nb-text-xl">per mo</span>
+              </h1>
+              <Link to="" className="">
+                <div className="nb-h-12 nb-rounded  nb-border-primary-red nb-border-2 nb-p-2 nb-text-xl nb-font-bold nb-bg-primary-red nb-text-white nb-cursor-pointer nb-text-center">
+                  View Plans
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 export default PricingStandard;
--- src/pages/hosting/LinuxSharedHosting.jsx
+++ src/pages/hosting/LinuxSharedHosting.jsx
@@ -1,9 +1,161 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 function LinuxSharedHosting() {
     return(
         <>
-            <h1>Linux Shared Hosting</h1>
+        <section className="nb-py-8 nb-bg-gray-100">
+            <div className="nb-container nb-p-4">
+                <ul className="nb-flex nb-items-center nb-justify-center">
+                    <li className="nb-flex nb-items-center nb-border-2 nb-border-r nb-border-primary-blue nb-py-2 nb-px-6 nb-bg-primary-blue nb-text-white hover:nb-bg-primary-blue hover:nb-text-white nb-cursor-pointer nb-font-bold"><img className="nb-mr-1 nb-h-6" src="/images/flag/ind.svg" alt="India Flag" />India</li>
+                    <li className="nb-flex nb-items-center nb-border-2 nb-border-l nb-border-r nb-border-primary-blue nb-py-2 nb-px-6 nb-text-primary-blue hover:nb-bg-primary-blue hover:nb-text-white nb-cursor-pointer nb-font-bold"><img className="nb-mr-1 nb-h-6" src="/images/flag/usa.svg" alt="USA Flag" />USA</li>
+                    <li className="nb-flex nb-items-center nb-border-2 nb-border-l nb-border-r nb-border-primary-blue nb-py-2 nb-px-6 nb-text-primary-blue hover:nb-bg-primary-blue hover:nb-text-white nb-cursor-pointer nb-font-bold"><img className="nb-mr-1 nb-h-6" src="/images/flag/uk.svg" alt="UK Flag" />United Kingdom</li>
+                    <li className="nb-flex nb-items-center nb-border-2 nb-border-l nb-border-primary-blue nb-py-2 nb-px-6 nb-text-primary-blue hover:nb-bg-primary-blue hover:nb-text-white nb-cursor-pointer nb-font-bold"><img className="nb-mr-1 nb-h-6" src="/images/flag/hk.svg" alt="Hong Kong Flag" />Hong Kong</li>
+                </ul>
+                <div className="">
+                    <p className="nb-text-md lg:nb-text-xl nb-font-semibold nb-text-primary-bunty nb-tracking-wide nb-my-6 nb-text-center">Best suited for visitors from US & world over</p>
+
+                    <div className="nb-grid nb-grid-cols-4 nb-gap-4">
+                        
+                        <div className="nb-border-2 nb-border-primary-blue nb-p-4 nb-rounded">
+                            <h1 className="nb-text-2xl nb-font-bold nb-text-primary-bunty nb-text-center">
+                                SDH-Linux-India Plan1
+                            </h1>
+                            <h2 className="nb-text-sm nb-font-semibold nb-opacity-75 nb-text-primary-blue nb-text-center">
+                                Free WHMCS Included
+                            </h2>
+                            <ul className="nb-mt-8 nb-mb-4">
+                                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                                <p className="nb-text-sm">99.9% Uptime Guarantee</p>
+                                </li>
+                                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                                <p className="nb-text-sm">WHM for Management</p>
+                                </li>
+                                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                                <p className="nb-text-sm">Softaculous Included</p>
+                                </li>
+                            </ul>
+                            <h2 className="nb-text-sm nb-font-semibold nb-text-primary-blue nb-text-center nb-opacity-75">
+                                Starting From
+                            </h2>
+                            <h1 className="nb-mb-4 nb-text-3xl nb-font-semibold nb-text-primary-blue nb-text-center">
+                                <span className="nb-text-xl">₹</span>
+                                1,080.00
+                                <span className="nb-text-xl">per mo</span>
+                            </h1>
+                            <Link to="" className="">
+                                <div className="nb-h-12 nb-rounded nb-border-primary-red nb-border-2 nb-p-2 nb-text-xl nb-font-bold nb-bg-primary-red nb-text-white nb-cursor-pointer nb-text-center">
+                                View Plans
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className="nb-border-2 nb-border-primary-blue nb-p-4 nb-rounded">
+                            <h1 className="nb-text-2xl nb-font-bold nb-text-primary-bunty nb-text-center">
+                                SDH-Linux-India Plan2
+                            </h1>
+                            <h2 className="nb-text-sm nb-font-semibold nb-opacity-75 nb-text-primary-blue nb-text-center">
+                                Free WHMCS Included
+                            </h2>
+                            <ul className="nb-mt-8 nb-mb-4">
+                                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                                <p className="nb-text-sm">99.9% Uptime Guarantee</p>
+                                </li>
+                                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                                <p className="nb-text-sm">WHM for Management</p>
+                                </li>
+                                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                                <p className="nb-text-sm">Softaculous Included</p>
+                                </li>
+                            </ul>
+                            <h2 className="nb-text-sm nb-font-semibold nb-text-primary-blue nb-text-center nb-opacity-75">
+                                Starting From
+                            </h2>
+                            <h1 className="nb-mb-4 nb-text-3xl nb-font-semibold nb-text-primary-blue nb-text-center">
+                                <span className="nb-text-xl">₹</span>
+                                1,080.00
+                                <span className="nb-text-xl">per mo</span>
+                            </h1>
+                            <Link to="" className="">
+                                <div className="nb-h-12 nb-rounded nb-border-primary-red nb-border-2 nb-p-2 nb-text-xl nb-font-bold nb-bg-primary-red nb-text-white nb-cursor-pointer nb-text-center">
+                                View Plans
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className="nb-border-2 nb-border-primary-blue nb-p-4 nb-rounded">
+                            <h1 className="nb-text-2xl nb-font-bold nb-text-primary-bunty nb-text-center">
+                                MDH-Linux-India Plan3
+                            </h1>
+                            <h2 className="nb-text-sm nb-font-semibold nb-opacity-75 nb-text-primary-blue nb-text-center">
+                                Free WHMCS Included
+                            </h2>
+                            <ul className="nb-mt-8 nb-mb-4">
+                                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                                <p className="nb-text-sm">99.9% Uptime Guarantee</p>
+                                </li>
+                                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                                <p className="nb-text-sm">WHM for Management</p>
+                                </li>
+                                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                                <p className="nb-text-sm">Softaculous Included</p>
+                                </li>
+                            </ul>
+                            <h2 className="nb-text-sm nb-font-semibold nb-text-primary-blue nb-text-center nb-opacity-75">
+                                Starting From
+                            </h2>
+                            <h1 className="nb-mb-4 nb-text-3xl nb-font-semibold nb-text-primary-blue nb-text-center">
+                                <span className="nb-text-xl">₹</span>
+                                1,080.00
+                                <span className="nb-text-xl">per mo</span>
+                            </h1>
+                            <Link to="" className="">
+                                <div className="nb-h-12 nb-rounded nb-border-primary-red nb-border-2 nb-p-2 nb-text-xl nb-font-bold nb-bg-primary-red nb-text-white nb-cursor-pointer nb-text-center">
+                                View Plans
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className="nb-border-2 nb-border-primary-blue nb-p-4 nb-rounded">
+                            <h1 className="nb-text-2xl nb-font-bold nb-text-primary-bunty nb-text-center">
+                                MDH-Linux-India Plan4
+                            </h1>
+                            <h2 className="nb-text-sm nb-font-semibold nb-opacity-75 nb-text-primary-blue nb-text-center">
+                                Free WHMCS Included
+                            </h2>
+                            <ul className="nb-mt-8 nb-mb-4">
+                                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                                <p className="nb-text-sm">99.9% Uptime Guarantee</p>
+                                </li>
+                                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                                <p className="nb-text-sm">WHM for Management</p>
+                                </li>
+                                <li className="pricing-icon-tick nb-text-lg nb-text-primary-bunty">
+                                <p className="nb-text-sm">Softaculous Included</p>
+                                </li>
+                            </ul>
+                            <h2 className="nb-text-sm nb-font-semibold nb-text-primary-blue nb-text-center nb-opacity-75">
+                                Starting From
+                            </h2>
+                            <h1 className="nb-mb-4 nb-text-3xl nb-font-semibold nb-text-primary-blue nb-text-center">
+                                <span className="nb-text-xl">₹</span>
+                                1,080.00
+                                <span className="nb-text-xl">per mo</span>
+                            </h1>
+                            <Link to="" className="">
+                                <div className="nb-h-12 nb-rounded nb-border-primary-red nb-border-2 nb-p-2 nb-text-xl nb-font-bold nb-bg-primary-red nb-text-white nb-cursor-pointer nb-text-center">
+                                View Plans
+                                </div>
+                            </Link>
+                        </div>
+
+                    </div>
+                
+                    <p className="nb-text-md lg:nb-text-xl nb-font-semibold nb-text-primary-bunty nb-tracking-wide nb-my-6 nb-text-center">If not satisfied, get your money back by cancelling within 30 days</p>
+                
+                </div>
+            </div>
+        </section>
         </>
     )
 }
